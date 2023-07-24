a1bfe39cd5f7af73ce3775bba1caaf252319d2f6
HappyTTian <tianhy20@gmail.com>
2023-07-24 05:15:10 -0500

    modify game styles

--- wordle.py
+++ wordle.py
@@ -1,8 +1,15 @@
 import pathlib
 import random
 from string import ascii_letters
+from rich.console import Console
 
-#get a random word for the game
+console = Console()
+console.print("hello [bold red]rich[/] :snake: ")
+
+def refresh(headline):
+    console.clear()
+    console.rule(f"[bold red] :leafy_green: {headline} :leafy_green: [/] \n")
+
 def get_random_word(file):
     word_length = 5 #the length of word we guess
 
@@ -16,35 +23,46 @@ def get_random_word(file):
     return answer
 
 #show which letters are guessed right and which are wrong
-def display_guess (guess, answer):
+def display_guess (guess_list, answer):
     """Show the user's guess on the terminal and classify all letters.
-
-    ## Example:
-
-    >>> display_guess("CRANE", "SNAKE")
-    Correct letters: A, E
-    Misplaced letters: N
-    Wrong letters: C, R
     """
 
-    correct_letters = {c1 for c1,c2 in zip(answer,guess) if c1 == c2}
-    misplace_letters = set(answer) & set(guess) - correct_letters
-    wrong_letters = set(guess) - set(answer)
-    print ("Correct letters: ", ", ".join(correct_letters))
-    print ("Misplaced letters: ", ", ".join(misplace_letters))
-    print ("Wrong letters: ",", ".join(wrong_letters))
+    # correct_letters = {c1 for c1,c2 in zip(answer,guess) if c1 == c2}
+    # misplace_letters = set(answer) & set(guess) - correct_letters
+    # wrong_letters = set(guess) - set(answer)
+    # print ("Correct letters: ", ", ".join(correct_letters))
+    # print ("Misplaced letters: ", ", ".join(misplace_letters))
+    # print ("Wrong letters: ",", ".join(wrong_letters))
+
+    for guess in guess_list:
+        styles_letters = []
+        for letter, correct in zip(guess,answer):
+            if letter == correct:
+                style = "bold white on green"
+            elif letter in answer:
+                style = "bold white on yellow"
+            elif letter in ascii_letters:
+                style = "white on #666666"
+            else:
+                style = "dim"
+            styles_letters.append(f"[{style}]{letter}[/]")
+        console.print("".join(styles_letters),justify="center")
 
 def game_over (word):
     print (f"The word was {word}")
 
 def main():
-    answer = get_random_word("wordList.txt")
+    answer = get_random_word("/home/kool/Wordle/wordList.txt")
+    guess_list = ["_"*5]*6
+
     for i in range(6):
-        guess = input(f"Guess {i+1}: ").lower()
-        if guess == answer:
+        refresh(headline=f"Guess{i+1}")
+        display_guess(guess_list,answer)
+        guess_list[i] = input(f"\nGuess {i+1}: ").lower()
+        
+        if guess_list[i] == answer:
             print ("Correct")
             break
-        display_guess(guess,answer)
     else:
         game_over(answer)
    
